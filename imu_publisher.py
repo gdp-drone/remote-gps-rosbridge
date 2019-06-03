8e984b03e0cbb426808f6d2f777297d1e7cf771b
Gerald Low <bcl15@ic.ac.uk>
2019-06-03 10:27:36 +0100

    Edited to flow w/o binaries

--- imu_publisher.py
+++ imu_publisher.py
@@ -17,9 +17,18 @@ def start_listening():
     imu_listener.subscribe(imu_recv)
 
 def imu_recv(message):
-    imu_transmit['orientation'] = message['orientation'] # TODO: unsure if subscript works
-    imu_transmit['angular_velocity'] = message['angular_velocity']
-    imu_transmit['linear_acceleration'] = message['linear_acceleration']
+    imu_transmit['orientation']['x'] = message['orientation']['x']
+    imu_transmit['orientation']['y'] = message['orientation']['y']
+    imu_transmit['orientation']['z'] = message['orientation']['z']
+    imu_transmit['orientation']['w'] = message['orientation']['w']
+
+    imu_transmit['angular_velocity']['x'] = message['angular_velocity']['x']
+    imu_transmit['angular_velocity']['y'] = message['angular_velocity']['y']
+    imu_transmit['angular_velocity']['z'] = message['angular_velocity']['z']
+
+    imu_transmit['linear_acceleration']['x'] = message['linear_acceleration']['x']
+    imu_transmit['linear_acceleration']['y'] = message['linear_acceleration']['y']
+    imu_transmit['linear_acceleration']['z'] = message['linear_acceleration']['z']
     print(imu_transmit)
 
 def start_talking():
